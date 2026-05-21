07149004eb7d3e6be2c11ea982a0d34bddfaa4aa
Revanth Settipalli <revanthsettipallii@gmail.com>
2026-05-22 04:32:29 +0530

    Improve prediction training dataset

--- backend/prediction/train_model.py
+++ backend/prediction/train_model.py
@@ -1,21 +1,35 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import joblib
+from sklearn.linear_model import LinearRegression
 
-FILE = "data/processed/weather_clean.csv"
 
-df = pd.read_csv(FILE)
+data = pd.DataFrame(
+{
+"humidity":[
+30,35,40,45,50,
+55,60,65,70,75,80
+],
 
-X = df[["humidity"]]
-y = df["temperature"]
+"temperature":[
+39,38,37,36,35,
+34,33,32,31,30,29
+]
+}
+)
+
+X = data[["humidity"]]
+
+y = data["temperature"]
 
 model = LinearRegression()
 
-model.fit(X, y)
+model.fit(X,y)
 
 joblib.dump(
-    model,
-    "models/weather/weather_model.pkl"
+model,
+"models/weather/weather_model.pkl"
 )
 
-print("\nModel trained successfully")
+print(
+"UrbanMind model retrained"
+)
